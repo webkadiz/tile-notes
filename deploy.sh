f31137a375a7a62c00a441d7ed21eafb23bf0882
Vladislav Tkachenko <webkadiz@gmail.com>
2022-09-25 19:02:00 +0300

    tune deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,19 @@
 
 cd /tmp
 git clone git@github.com:webkadiz/tile-notes.git
+test $? -ne 0 && exit 
+
 cd tile-notes/client
 npm i
 PUBLIC_PATH=. npm run build
 cp -r build /tmp/tile-notes-build
-cd ..
+
+cd  /tmp/tile-notes
+test $? -ne 0 && exit 
+
 git checkout gh-pages
-rm -rf *
-mv /tmp/tile-notes-build/* .
+rm -rf /tmp/tile-notes/*
+mv /tmp/tile-notes-build/* /tmp/tile-notes
 git add .
 git commit -m deploy
 git push
